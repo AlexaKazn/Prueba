9500ca697c709781241b61ff149dfde5f56cd60e
AlexaKazn <pnunez0314@uta.edu.ec>
2025-05-09 17:03:38 -0500

    PAOLA NUNEZ: 17:03

---
+++ lib/pages/ventanaUnoNDPA.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class VentanaUnoNDPA extends StatefulWidget {
+  const VentanaUnoNDPA({super.key});
+
+  @override
+  State<VentanaUnoNDPA> createState() => _VentanaUnoState();
+}
+
+class _VentanaUnoState extends State<VentanaUnoNDPA> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
